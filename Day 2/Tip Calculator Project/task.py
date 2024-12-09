c315d5fe559fe3775a87381e4ae06b89b58f39e5
Fatma Shalaby <fatmaa.shalabyy@gmail.com>
2024-12-10 00:32:03 +0100

    Removed faulty print statement

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -4,5 +4,4 @@ tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 final_amount = round(((bill * (tip / 100)) + bill) / people , 2)
 print(f"Each person should pay: ${final_amount}")
-print("tizak kebira")
 

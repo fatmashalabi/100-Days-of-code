0092a0180e95dbe88d97066dc056d875e45fdbe4
Fatma Shalaby <fatmaa.shalabyy@gmail.com>
2024-12-10 22:24:07 +0100

    Removed faulty print statement

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,6 @@
+#Checks whether the number is odd or even
+number = int(input("Write a number "))
+if number % 2 == 0 :
+    print("number is even")
+else:
+    print("number is odd")
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,29 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+if size == "S":
+    bill = 15
+    if pepperoni == "Y":
+        bill +=2
+    if extra_cheese == "Y":
+        bill +=1
+elif size == "M":
+    bill = 20
+    if pepperoni == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+else: #L:= large pizza
+    bill = 25
+    if pepperoni == "Y":
+        bill +=3
+    if extra_cheese == "Y":
+        bill += 1
+print("Your final bill is: $" + str(bill))
+
+
+
+
+
+
+

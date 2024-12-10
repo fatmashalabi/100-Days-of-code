8f314ba3d1a8089463c3c7b3d5c7a67e0eae34c8
Fatma Shalaby <fatmaa.shalabyy@gmail.com>
2024-12-11 00:07:09 +0100

    Removed faulty print statement

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -1,3 +1,5 @@
+from nis import match
+
 print(r'''
 *******************************************************************************
           |                   |                  |                     |
@@ -22,3 +24,25 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+left_or_right = str(input("left or right? ")).lower().strip() #strip for spaces and tabs
+if(left_or_right != "left"):
+    print("Fall into a hole. Game Over")
+elif(left_or_right == "left"): #in := gives true for all the substrings in "left"
+    swim_or_wait = str(input("Would you like to swim or wait? ")).lower().strip()
+    if(swim_or_wait != "wait"):
+        print("You are attacked by trout. Game Over.")
+    elif(swim_or_wait == "wait"):
+        door = str(input("Which door would you like to go, red, blue or yellow ? ")).lower().strip()
+        if(door == "red"):
+            print("Burned by fire. Game Over.")
+        elif(door == "blue"):
+            print("Eaten by beasts. Game Over.")
+        elif(door == "yellow"):
+            print("You Win!")
+        else:
+            print("Game Over.")
+
+
+str.
+
+
